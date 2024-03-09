93c8a55eaef305d853d1fa22a757ba18600cf57c
Daniel <danielkioko1844@gmail.com>
2024-03-09 03:27:46 +0300

    Debug

--- projectsApp/urls.py
+++ projectsApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -42,3 +44,5 @@ urlpatterns = [
 
     path('logout/', views.logout_view, name='logout'),
 ]
+# uploads url#
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
